3982afd8f28a7498ead975d1413baae9428f563f
Yudayana Arif Prasojo <yuda.arif.prasojo@gmail.com>
2024-09-04 15:25:54 +0700

    feat: main page and unit test

--- main/views.py
+++ main/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+def show_main(request):
+    context = {
+        'npm' : '2306215160',
+        'name': 'Yudayana Arif Prasojo',
+        'class': 'PBP D'
+    }
+
+    return render(request, "main.html", context)
