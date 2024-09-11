cf27505321ee4d87519f32213d648c1bd3067492
Yudayana Arif Prasojo <yuda.arif.prasojo@gmail.com>
2024-09-11 15:34:23 +0700

    feat: show data by id in json and xml

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from main.views import show_main, create_mood_entry, show_xml, show_json
+from main.views import show_main, create_mood_entry, show_xml, show_json, show_json_by_id, show_xml_by_id
 
 app_name = 'main'
 
@@ -7,5 +7,7 @@ urlpatterns = [
     path('', show_main, name='show_main'),
     path('create-mood-entry', create_mood_entry, name='create_mood_entry'),
     path('xml/', show_xml, name='show_xml'),
-    path('json/', show_json, name='show_json')
+    path('json/', show_json, name='show_json'),
+    path('xml/<str:id>/', show_xml_by_id, name='show_xml_by_id'),
+    path('json/<str:id>/', show_json_by_id, name='show_json_by_id'),
 ]
--- main/views.py
+++ main/views.py
@@ -12,6 +12,14 @@ def show_json(request):
     data = MoodEntry.objects.all()
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
 
+def show_xml_by_id(request, id):
+    data = MoodEntry.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json_by_id(request, id):
+    data = MoodEntry.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
 def create_mood_entry(request):
     form = MoodEntryForm(request.POST or None)
     
