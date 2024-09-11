1a2d9f3e216d7d32b5dcabc0043c2f39e856203a
Yudayana Arif Prasojo <yuda.arif.prasojo@gmail.com>
2024-09-11 15:25:57 +0700

    feat: add basic form functionality

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from main.views import show_main
+from main.views import show_main, create_mood_entry
 
 app_name = 'main'
 
 urlpatterns = [
     path('', show_main, name='show_main'),
+    path('create-mood-entry', create_mood_entry, name='create_mood_entry')
 ]
--- main/views.py
+++ main/views.py
@@ -1,10 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from main.forms import MoodEntryForm
+from main.models import MoodEntry
+
+def create_mood_entry(request):
+    form = MoodEntryForm(request.POST or None)
+    
+    if form.is_valid() and request.method == "POST":
+        form.save()
+        return redirect("main:show_main")
+    
+    context = {
+        "form": form
+    }
+    
+    return render(request, "create_mood_entry.html", context)
 
 def show_main(request):
+    mood_entries = MoodEntry.objects.all()
     context = {
         'npm' : '2306215160',
         'name': 'Yudayana Arif Prasojo',
-        'class': 'PBP D'
+        'class': 'PBP D',
+        'mood_entries': mood_entries
     }
 
     return render(request, "main.html", context)
