47a1ec70af0f8d24708f12786b9fde8dcf070d05
Yudayana Arif Prasojo <yuda.arif.prasojo@gmail.com>
2024-09-18 15:32:30 +0700

    feat: add register and login functionality

--- main/views.py
+++ main/views.py
@@ -1,45 +1,90 @@
-from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.core import serializers
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
+
 from main.forms import MoodEntryForm
 from main.models import MoodEntry
 
+
 def show_xml(request):
     data = MoodEntry.objects.all()
-    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+    return HttpResponse(
+        serializers.serialize("xml", data), content_type="application/xml"
+    )
+
 
 def show_json(request):
     data = MoodEntry.objects.all()
-    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+    return HttpResponse(
+        serializers.serialize("json", data), content_type="application/json"
+    )
+
 
 def show_xml_by_id(request, id):
     data = MoodEntry.objects.filter(pk=id)
-    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+    return HttpResponse(
+        serializers.serialize("xml", data), content_type="application/xml"
+    )
+
 
 def show_json_by_id(request, id):
     data = MoodEntry.objects.filter(pk=id)
-    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+    return HttpResponse(
+        serializers.serialize("json", data), content_type="application/json"
+    )
+
 
 def create_mood_entry(request):
     form = MoodEntryForm(request.POST or None)
-    
+
     if form.is_valid() and request.method == "POST":
         form.save()
         return redirect("main:show_main")
-    
-    context = {
-        "form": form
-    }
-    
+
+    context = {"form": form}
+
     return render(request, "create_mood_entry.html", context)
+
 
 def show_main(request):
     mood_entries = MoodEntry.objects.all()
     context = {
-        'npm' : '2306215160',
-        'name': 'Yudayana Arif Prasojo',
-        'class': 'PBP D',
-        'mood_entries': mood_entries
+        "npm": "2306215160",
+        "name": "Yudayana Arif Prasojo",
+        "class": "PBP D",
+        "mood_entries": mood_entries,
     }
 
     return render(request, "main.html", context)
+
+
+def register(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Your account has been successfully created!")
+            return redirect("main:login")
+    context = {"form": form}
+    return render(request, "register.html", context)
+
+
+def login_user(request):
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect("main:show_main")
+
+    else:
+        form = AuthenticationForm(request)
+    context = {"form": form}
+    return render(request, "login.html", context)
+
